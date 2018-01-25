680ece7cb35312a44d326a6c0d866e9eb15a8e2a
Pato2790 <pato.sartore@gmail.com>
2018-01-25 11:41:35 -0300

    routeo Camiones-Empresas-CamionesEmpresas

---
+++ routes/camion.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+var model = require('../models/index');
+ 
+router.get('/', function(req, res, next) {
+	model.camions.findAll({})
+        .then(camiones => res.json({
+            error: false,
+            data: camiones
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
+});
+ 
+router.post('/', function(req, res, next) {
+
+    const {
+        patenteChasis,
+        patenteAcoplado,
+        nombreChofer,
+    } = req.body;
+    
+    model.camions.create({
+            patenteChasis: patenteChasis,
+            patenteAcoplado: patenteAcoplado,
+            nombreChofer: nombreChofer,
+        })
+        .then(newCamion => res.status(201).json({
+            error: false,
+            message: 'Se ha ingresado correctamente el nuevo camion'
+        }))
+        .catch(error => res.json({
+            error: true,
+            message: 'Hubo un error al intentar cargar un nuevo camion. Contactese con el administrador.'
+        }));
+});
+ 
+router.put('/:id', function(req, res, next) {
+ 
+    const camion_id = req.params.id;
+ 
+    const {
+        patenteChasis,
+        patenteAcoplado,
+        nombreChofer,
+    } = req.body;
+ 
+    model.camions.update({
+            patenteChasis: patenteChasis,
+            patenteAcoplado: patenteAcoplado,
+            nombreChofer: nombreChofer
+        }, {
+            where: {
+                id: camion_id
+            }
+        })
+        .then(updateCamion => res.status(201).json({
+            error: false,
+            message: 'Se han actualizado los datos del camion correctamente'
+        }))
+        .catch(error => res.json({
+            error: true,
+            error: error
+        }));
+});
+ 
+router.delete('/:id', function(req, res, next) {
+
+    const camion_id = req.params.id;
+ 
+    model.camions.destroy({ 
+        where: {
+            id: camion_id
+        }
+    })
+    .then(status => res.status(201).json({
+        error: false,
+        message: 'El camion ha sido eliminado'
+    }))
+    .catch(error => res.json({
+        error: true,
+        error: error
+    }));
+});
+ 
+module.exports = router;
---
+++ routes/empresa.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var router = express.Router();
+var model = require('../models/index');
+ 
+router.get('/', function(req, res, next) {
+	model.empresas.findAll({})
+        .then(empresas => res.json({
+            error: false,
+            data: empresas
+        }))
+        .catch(error => res.json({
+            error: true,
+            data: [],
+            error: error
+        }));
+});
+ 
+router.post('/', function(req, res, next) {
+
+    const {
+        nombre,
+        direccion,
+        telefono,
+        camiones_ids
+    } = req.body;
+
+    console.log(camiones_ids);
+    
+    model.empresas.create({
+            nombre: nombre,
+            direccion: direccion,
+            telefono: telefono,
+        })
+        .then(newEmpresa => 
+            camiones_ids.forEach(function(valor, indice){
+                model.camionempresas.create({
+                    camionId: valor,
+                    empresaId: newEmpresa.id
+                })
+                .then(camionEmpresa => console.log("Asociación Camion-Empresa realizada correctamente."))
+                .catch(error => console.log(error))
+            }
+        ))
+        .then(newEmpresa => res.status(201).json({
+            error: false,
+            message: 'Se ha ingresado correctamente la nueva empresa'
+        }))
+        .catch(error => res.json({
+            error: true,
+            message: 'Hubo un error al intentar cargar una nueva empresa. Contactese con el administrador.'
+        }));
+});
+ 
+router.put('/:id', function(req, res, next) {
+ 
+    const empresa_id = req.params.id;
+ 
+    const { nombre, direccion, telefono } = req.body;
+ 
+    model.empresas.update({
+            nombre: nombre,
+            direccion: direccion,
+            telefono: telefono
+        }, {
+            where: {
+                id: empresa_id
+            }
+        })
+        .then(updateEmpresa => res.status(201).json({
+            error: false,
+            message: 'Se han actualizado los datos de la empresa correctamente'
+        }))
+        .catch(error => res.json({
+            error: true,
+            error: error
+        }));
+});
+ 
+router.delete('/:id', function(req, res, next) {
+
+    const empresa_id = req.params.id;
+ 
+    model.empresas.destroy({ 
+        where: {
+            id: empresa_id
+        }
+    })
+    .then(status => res.status(201).json({
+        error: false,
+        message: 'La empresa ha sido eliminada'
+    }))
+    .catch(error => res.json({
+        error: true,
+        error: error
+    }));
+});
+ 
+module.exports = router;
